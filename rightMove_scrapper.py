e150a02349c1cfd4ac927deaae558a08ddb3aaae
Olaoluwa <olaoluwaidowu22@gmail.com>
2023-04-20 18:00:11 +0100

    updated rightmove scrapper

--- rightMove_scrapper.py
+++ rightMove_scrapper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import requests
+from district import postcode
 
 
 def get_data(num_properties = 1500):
