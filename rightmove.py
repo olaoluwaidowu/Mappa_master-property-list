1be51b258093a57019f97921ef5a4f432a53462b
Olaoluwa <olaoluwaidowu22@gmail.com>
2023-05-23 20:25:00 +0100

    updated rightmove.py

--- rightmove.py
+++ rightmove.py
@@ -11,8 +11,9 @@ import pandas as pd
 
 
 
-def get_data(num_properties = 20,verbose=True):
+def get_data(num_properties = 500,verbose=True):
     options = Options()
+    #options.add_argument("--headless")
     options.add_argument("start-maximized")
     driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
 
@@ -23,84 +24,90 @@ def get_data(num_properties = 20,verbose=True):
 
     properties = []
     
-        #time.sleep(10)
     for code in postcode:
         while len(properties) < (num_properties+1):
-            driver.get(url)
-            print("\nstarting new area")
-            search_box = driver.find_element(By.XPATH, '//input[@name="typeAheadInputField"]').send_keys(code)
-            submit = driver.find_element(By.XPATH, '//*[@id="HomeDesktopLayout_searchPanel__vTqkA"]/div/div/div/button[1]').click()
-            driver.find_element(By.XPATH, '//button[@id="submit"]').click()
             
-            while True:
+            print("\nstarting new area")
+            
+            for i in range(2):
+                driver.get(url)
+                print("\nstarting new property type")
+                search_box = driver.find_element(By.XPATH, '//input[@name="typeAheadInputField"]').send_keys(code)
+                time.sleep(3)
+                if i == 0:
+                    Transaction_type = "Sale"
+                    driver.find_element(By.XPATH, '//*[@id="HomeDesktopLayout_searchPanel__vTqkA"]/div/div/div/button[1]').click()
+                elif i == 1:
+                    Transaction_type = "Rent"
+                    driver.find_element(By.XPATH, '//*[@id="HomeDesktopLayout_searchPanel__vTqkA"]/div/div/div/button[2]').click()
+            
+                driver.find_element(By.XPATH, '//button[@id="submit"]').click()
+                
+                while True:
 
-                property = driver.find_elements(By.CLASS_NAME, "propertyCard-wrapper")
-                print(len(property))
-                prop = driver.find_element(By.XPATH, '//div[@class="l-searchResult is-list"]')
-                time.sleep(5)
-                #prop_buttons = prop.find_elements(By.CLASS_NAME)
-                print("success")
-                #print(address)
-                for p in property:
+                    property = driver.find_elements(By.CLASS_NAME, "propertyCard-wrapper")
+                    print(len(property))
+                    #prop = driver.find_element(By.XPATH, '//div[@class="l-searchResult is-list"]')
+                    time.sleep(5)
+                    #prop_buttons = prop.find_elements(By.CLASS_NAME)
+                    print("success")
+                    #print(address)
+                    for p in property:
 
-                    try:
-                        print("success 6")
-                        # get location, price and description
-                        time.sleep(10)
-                        location = p.find_element(By.CLASS_NAME, "propertyCard-address")
-
-
-                        
-                        print("success 7")
-                        price = p.find_element(By.CLASS_NAME, "propertyCard-priceValue").text
-                        desc = p.find_element(By.CLASS_NAME, "propertyCard-description").text
-                        agent = p.find_element(By.CLASS_NAME, "propertyCard-branchSummary").text
-                        #propertyCard-branchSummary property-card-updates
-                        a = p.find_element(By.TAG_NAME, "a")
-                        listing_url = a.get_attribute("href")
-
-                        print(agent)
-                        print(listing_url)
+                        try:
+                            # get property details
+                            time.sleep(5)
+                            location = p.find_element(By.CLASS_NAME, "propertyCard-address").text
+                            price = p.find_element(By.CLASS_NAME, "propertyCard-priceValue").text
+                            desc = p.find_element(By.CLASS_NAME, "propertyCard-description").text
+                            agent = p.find_element(By.CLASS_NAME, "propertyCard-branchSummary").text
                             
+                            a = p.find_element(By.TAG_NAME, "a")
+                            listing_url = a.get_attribute("href")
 
-                        # get type, bedroom and toilet of property
+                            information = p.find_element(By.CLASS_NAME, "property-information").text
+
+                            information = information.split("\n")
+                            type = information[0]
+                            if len(information) < 2:
+                                bedroom = toilet = "NA"
+                            elif len(information) < 3:
+                                bedroom = information[1]
+                                toilet = "NA"
+                            else:
+                                bedroom = information[1]
+                                toilet = information[2]
                             
-                        information = p.find_element(By.CLASS_NAME, "property-information").text
+                                
 
-                        information = information.split("\n")
-                        type = information[0]
-                        if len(information) < 2:
-                            bedroom = toilet = "NA"
-                        elif len(information) < 3:
-                            bedroom = information[1]
-                            toilet = "NA"
-                        else:
-                            bedroom = information[1]
-                            toilet = information[2]
+                            print(f"length of info {len(information)}")
 
-                        print(f"length of info {len(information)}")
+                            properties.append({"Transaction_type" : Transaction_type,
+                            "Bedroom" : bedroom,
+                            "Bathrooms" : toilet,
+                            "Description" : desc,
+                            "Property_type" : type,
+                            "Price" : price,
+                            "Location" : location,
+                            "Agent" : agent,
+                            "Listing_url" : listing_url})
 
-                        properties.append({"Type" : type,
-                        "Description" : desc,
-                        "Bedroom" : bedroom,
-                        "Toilet" : toilet,
-                        "Agent" : agent,
-                        "Listing_url" : listing_url,
-                        "Price" : price})
+                            print(f"Scraped {len(properties)} properties.")
+                        except NoSuchElementException:
+                            print("Failed to scrape property information.")
+                        #if len(properties) > 10:
+                            #break
+                    if len(properties) > 500:
+                        break
+
+                    if verbose:
                         print(f"Scraped {len(properties)} properties.")
-                    except NoSuchElementException:
-                        print("Failed to scrape property information.")
-                if len(properties) > 49:
-                    break
-
-                if verbose:
-                    print(f"Scraped {len(properties)} properties.")
-                next_button = driver.find_element(By.XPATH, '//button[@class="pagination-button pagination-direction pagination-direction--next"]')
-                if not next_button.is_enabled():
-                    print("No more pages available.")
-                    break
-                next_button.click()
-                time.sleep(5)
+                    next_button = driver.find_element(By.XPATH, '//button[@class="pagination-button pagination-direction pagination-direction--next"]')
+                    if not next_button.is_enabled():
+                        print("No more pages available.")
+                        break
+                    next_button.click()
+                    time.sleep(5)
     return pd.DataFrame(properties).reset_index()
         
 df = get_data()
